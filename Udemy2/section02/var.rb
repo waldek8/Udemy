01fa8625050887f469732a13b23df02e0c357b37
Waldek Szejbak <waldunia@gmail.com>
2016-12-27 21:26:42 +0200

    second attempt

---
+++ Udemy2/section02/var.rb
@@ -0,0 +1,45 @@
+# name = "walduk"
+# address = ["26 street", "Supertown", "VLN"]
+# puts name
+# p name
+# puts address
+# p address
+# puts 150
+# p 150
+# 
+# puts "Type something:"
+# smth = gets.chomp
+# p smth
+
+#Local Variables
+# 10.times do
+#     x = 10
+#     p x
+# end
+
+#Global Variables
+# 10.times do
+#     $x = 10
+# end
+# 
+# p $x
+
+#Instance Variables
+#MVC thing, kurka. Controller --> View
+# @new_thing = 150
+# 
+# p @new_thing
+
+#Constant
+
+# ALLCAPITALLETTERS = "MY First Constant"
+# ALLCAPITALLETTERS = "MY Second Constant"
+# 
+# p ALLCAPITALLETTERS
+#Class Variables
+class WaldukaClass
+    @@teams = ['manchester', 'united']
+end
+
+#adding to git
+#modifying file
