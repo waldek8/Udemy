b49ee43b3a790d6906edc78d7a1efcf6ee306f63
Waldek Szejbak <waldunia@gmail.com>
2016-12-28 07:17:32 +0200

    adding section 3

---
+++ Udemy2/section03/temp.rb
@@ -0,0 +1,21 @@
+#x = "The quick brown fox jumped over the lazy dog"
+#p x
+
+# puts 'Give me the name'
+# name = gets.chomp
+
+# puts 'Give me age'
+# age = gets.chomp
+
+# puts "So... 6*6=#{6*6} "
+
+str = "Ele mele dudki, gospodarz malutki, mele mele ..."
+# puts str.gsub("mele", "trele")
+# puts str
+
+# puts str.gsub!("mele", "trele")
+# puts str
+
+# str = "     kurka matka    "
+# p str.strip
+p str.split(//)
