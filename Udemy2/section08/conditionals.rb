15ec9cfa4e43dd37b21c2848211edd36533a068c
Waldek Szejbak <waldunia@gmail.com>
2017-01-13 08:51:49 +0200

    section 08 finished

--- Udemy2/section08/conditionals.rb
+++ Udemy2/section08/conditionals.rb
@@ -9,3 +9,34 @@ end
 
 #unless
 puts "equal" unless x!=y
+
+#multiple
+x = 100
+y = 10
+
+if y < x
+	puts "y < x"
+elsif x == y
+	puts "y = x"
+else
+	puts "y > x"
+end
+
+#compounds
+z = 10
+
+if x != y && z == y
+	puts "coumpund rocks"
+end
+
+if x != y and z == y
+	puts "and coumpund rocks"
+end
+
+if x!=y || z==x
+	puts "pipes"
+end
+
+if x!=y or z==x
+	puts "or pipes"
+end
